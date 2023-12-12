a8196306ebaf2ccfada297b25f5c5b4f8f7cc21f
Artem Zlobin <111347458+zlobinartem@users.noreply.github.com>
2023-12-12 15:43:32 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -4,3 +4,7 @@ function getElementWidth (content, padding, border) {
     const totalPrice = (Number.parseFloat(content)) + (Number.parseFloat(padding)) * 2 + (Number.parseFloat(border)) * 2;
     return totalPrice;
   };
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
